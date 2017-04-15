cff22d899369aacebd3309af3eb86b13c0102126
Shannon Skipper <shannonskipper@gmail.com>
2017-04-15 15:27:18 -0700

    generate a Game channel

--- lib/trillium/web/channels/user_socket.ex
+++ lib/trillium/web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Trillium.Web.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Trillium.Web.RoomChannel
+  channel "room:*", Trillium.Web.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
