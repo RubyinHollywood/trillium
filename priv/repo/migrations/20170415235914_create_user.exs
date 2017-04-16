ce8fb93c039e441a72eef29e9cfeb44f4d2ccaa4
Stephanie Wilkinson <what.happens@gmail.com>
2017-04-15 17:04:21 -0700

    add user migration

--- priv/repo/migrations/20170415235914_create_user.exs
+++ priv/repo/migrations/20170415235914_create_user.exs
@@ -1,7 +1,9 @@
-defmodule Trillium.Repo.Migrations.CreateUser do
+defmodule Trillium.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add :email, :string
+      add :sets, :integer
   end
 end
