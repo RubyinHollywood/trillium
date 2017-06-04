aaff474a9d89ce43a9b72799083e9b0b622080d8
Shannon Skipper <shannonskipper@gmail.com>
2017-06-03 21:52:55 -0700

    add placeholder cards upon channel join

--- lib/trillium/web/channels/game_channel.ex
+++ lib/trillium/web/channels/game_channel.ex
@@ -3,9 +3,11 @@ defmodule Trillium.Web.GameChannel do
 
   alias Trillium.Set
 
+  @placeholder_cards ["example", "cards", "here"]
+
   def join("game", payload, socket) do
     if authorized?(payload) do
-      {:ok, socket}
+      {:ok, %{cards: @placeholder_cards}, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
