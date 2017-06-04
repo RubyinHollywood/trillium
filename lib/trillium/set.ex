aa56afac9d24dff29bbf5486ecb3ad6c86a119a8
Shannon Skipper <shannonskipper@gmail.com>
2017-06-03 21:42:32 -0700

    extract set functions to a Set module

---
+++ lib/trillium/set.ex
@@ -0,0 +1,47 @@
+defmodule Trillium.Set do
+  def valid?(payload) do
+    %{"body" => cards} = payload
+
+    three_cards?(cards)
+      and all_valid_dimensions?(cards)
+      and all_on_the_board?(cards)
+      and all_same_or_different?(cards)
+  end
+
+  defp three_cards?(cards) do
+    Enum.count(cards) == 3
+  end
+
+  defp all_valid_dimensions?(cards) do
+    cards
+    |> Enum.all?(&valid_dimensions?/1)
+  end
+
+  defp valid_dimensions?(card) do
+    ~r/[0-3]{4}/
+    |> Regex.match?(card)
+  end
+
+  defp all_on_the_board?(_cards) do
+    true
+  end
+
+  defp all_same_or_different?(cards) do
+    cards
+    |> Enum.map(&(String.codepoints(&1)))
+    |> Enum.zip
+    |> Enum.all?(&same_or_different?/1)
+  end
+
+  defp same_or_different?(dimensions) do
+    [1, tuple_size(dimensions)]
+    |> Enum.member?(count_unique_dimensions(dimensions))
+  end
+
+  defp count_unique_dimensions(dimensions) do
+    dimensions
+    |> Tuple.to_list
+    |> Enum.uniq
+    |> Enum.count
+  end
+end
--- lib/trillium/web/channels/game_channel.ex
+++ lib/trillium/web/channels/game_channel.ex
@@ -1,6 +1,8 @@
 defmodule Trillium.Web.GameChannel do
   use Trillium.Web, :channel
 
+  alias Trillium.Set
+
   def join("game", payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
@@ -10,7 +12,7 @@ defmodule Trillium.Web.GameChannel do
   end
 
   def handle_in("set", payload, socket) do
-    if valid_set? payload do
+    if Set.valid?(payload) do
       broadcast socket, "set", payload
     end
 
@@ -19,51 +21,5 @@ defmodule Trillium.Web.GameChannel do
 
   defp authorized?(_payload) do
     true
-  end
-
-  defp valid_set?(payload) do
-    %{"body" => cards} = payload
-
-    three_cards?(cards)
-      and all_valid_dimensions?(cards)
-      and all_on_the_board?(cards)
-      and all_same_or_different?(cards)
-  end
-
-  defp three_cards?(cards) do
-    Enum.count(cards) == 3
-  end
-
-  defp all_valid_dimensions?(cards) do
-    cards
-    |> Enum.all?(&valid_dimensions?/1)
-  end
-
-  defp valid_dimensions?(card) do
-    ~r/[0-3]{4}/
-    |> Regex.match?(card)
-  end
-
-  defp all_on_the_board?(_cards) do
-    true
-  end
-
-  defp all_same_or_different?(cards) do
-    cards
-    |> Enum.map(&(String.codepoints(&1)))
-    |> Enum.zip
-    |> Enum.all?(&same_or_different?/1)
-  end
-
-  defp same_or_different?(dimensions) do
-    [1, tuple_size(dimensions)]
-    |> Enum.member?(count_unique_dimensions(dimensions))
-  end
-
-  defp count_unique_dimensions(dimensions) do
-    dimensions
-    |> Tuple.to_list
-    |> Enum.uniq
-    |> Enum.count
   end
 end
