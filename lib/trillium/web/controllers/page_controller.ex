86ce3759053a6123b095d49a090f9d8134cd34bf
Shannon Skipper <shannonskipper@gmail.com>
2017-06-03 19:19:10 -0700

    remove example channel usage from the controller

--- lib/trillium/web/controllers/page_controller.ex
+++ lib/trillium/web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule Trillium.Web.PageController do
   use Trillium.Web, :controller
 
   def index(conn, _params) do
-    payload = %{message: "Pizza!!!"}
-    Trillium.Web.Endpoint.broadcast "game", "ping", %{body: Poison.encode!(payload)}
     render conn, "index.html"
   end
 end
